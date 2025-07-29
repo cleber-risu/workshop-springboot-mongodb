2dad9b7e7615e3b9c50376bef5f02bd86dbe9c87
cleber <cleber.risu@outlook.com>
2025-07-29 15:52:21 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/edu/workshopmongo/config/Instantiation.java
+++ src/main/java/com/edu/workshopmongo/config/Instantiation.java
@@ -1,12 +1,18 @@
 package com.edu.workshopmongo.config;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.edu.workshopmongo.domain.Post;
 import com.edu.workshopmongo.domain.User;
+import com.edu.workshopmongo.repository.PostRepository;
 import com.edu.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,16 +21,29 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null, criarInstantGMT(2018, 3, 21, 12, 0), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, criarInstantGMT(2018, 3, 23, 12, 0), "Bom dia", "Acordei feliz hoje!", maria);
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
+	}
+
+	public Instant criarInstantGMT(int ano, int mes, int dia, int hora, int minuto) {
+	    LocalDateTime ldt = LocalDateTime.of(ano, mes, dia, hora, minuto);
+	    return ldt.atZone(ZoneId.of("GMT")).toInstant();
 	}
 
 }
