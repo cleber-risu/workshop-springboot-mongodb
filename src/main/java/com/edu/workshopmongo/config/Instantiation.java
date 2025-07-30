4d4abb56ac85a69185c44c59128223ddaa2450f7
cleber <cleber.risu@outlook.com>
2025-07-30 10:56:29 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/edu/workshopmongo/config/Instantiation.java
+++ src/main/java/com/edu/workshopmongo/config/Instantiation.java
@@ -3,7 +3,6 @@ package com.edu.workshopmongo.config;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.edu.workshopmongo.domain.Post;
 import com.edu.workshopmongo.domain.User;
+import com.edu.workshopmongo.dto.AuthorDTO;
 import com.edu.workshopmongo.repository.PostRepository;
 import com.edu.workshopmongo.repository.UserRepository;
 
@@ -34,10 +34,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, criarInstantGMT(2018, 3, 21, 12, 0), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, criarInstantGMT(2018, 3, 23, 12, 0), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, criarInstantGMT(2018, 3, 21, 12, 0), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, criarInstantGMT(2018, 3, 23, 12, 0), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
